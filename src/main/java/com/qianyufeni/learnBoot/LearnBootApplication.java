c87ad9c8e57222ee615345aef84eb5f9f4de9944
qianyunfei <qianyunfei@trendytech.com.cn>
2018-05-10 10:46:56 +0800

    first commit

---
+++ src/main/java/com/qianyufeni/learnBoot/LearnBootApplication.java
@@ -0,0 +1,12 @@
+package com.qianyufeni.learnBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnBootApplication.class, args);
+	}
+}
